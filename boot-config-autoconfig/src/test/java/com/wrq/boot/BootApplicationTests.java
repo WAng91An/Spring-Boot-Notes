f8d8cd06c09df46783125943f2421849d4a77064
wangqian <1027700603@qq.com>
2019-01-13 20:51:04 +0800

    boot config autoconfig

---
+++ boot-config-autoconfig/src/test/java/com/wrq/boot/BootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wrq.boot;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootApplicationTests {
+
+
+
+}
+
